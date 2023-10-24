acf082a3d5548c24d2b7003e3d7cf997f59ef5d7
GineTik <densheva18072003@gmail.com>
2023-10-24 22:47:48 +0300

    Create UI layer

---
+++ UI/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace UI
+{
+    public partial class App : Application
+    {
+        public IHost AppHost { get; private set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((_, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost.StartAsync();
+
+            var startupWindow = AppHost.Services.GetRequiredService<MainWindow>();
+            startupWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost.StopAsync();
+            base.OnExit(e);
+        }
+    }
+}
---
+++ UI/ThemeChanger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+
+namespace UI;
+
+public enum Themes
+{
+    Light,
+    Dark,
+}
+
+public static class ThemeChanger
+{
+    public static void Change(Themes theme)
+    {
+        var resources = CreateResourceDictionaryByTheme(theme);
+        RemoveAllThemeResources();
+        Application.Current.Resources.MergedDictionaries.Add(resources);
+    }
+
+    private static void RemoveAllThemeResources()
+    {
+        var themeResources = new ResourceDictionary[]
+        {
+            CreateResourceDictionaryByTheme(Themes.Light),
+            CreateResourceDictionaryByTheme(Themes.Dark),
+        };
+        
+        foreach (var resource in themeResources)
+           Application.Current.Resources.MergedDictionaries.Remove(resource);
+    }
+
+    private static ResourceDictionary CreateResourceDictionaryByTheme(Themes theme)
+    {
+        var uri = new Uri($"Resources/Themes/{theme}.xaml", UriKind.Relative);
+        return new ResourceDictionary {Source = uri};
+    }
+}
