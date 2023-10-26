cdfacd02aee7fc1e2920352ebf4c51f900bc970c
GineTik <densheva18072003@gmail.com>
2023-10-26 18:38:22 +0300

    Made the table without styles to testing a logics

--- src/UI/Views/HomePage.xaml.cs
+++ src/UI/Views/HomePage.xaml.cs
@@ -1,11 +1,24 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using Domain.APIModels;
+using UI.ViewModels;
 
 namespace UI.Views;
 
 public partial class HomePage : Page
 {
-    public HomePage()
+    public HomePage(HomeViewModel homeViewModel)
     {
         InitializeComponent();
+        DataContext = homeViewModel;
     }
+
+    // public IEnumerable<ExchangeShort> Exhanges => new[]
+    // {
+    //     new ExchangeShort { Id = "binance", Name = "Binance" },
+    //     new ExchangeShort { Id = "test1", Name = "Test1" },
+    //     new ExchangeShort { Id = "test2", Name = "Test2" },
+    //     new ExchangeShort { Id = "test3", Name = "Test3" }
+    // };
 }
