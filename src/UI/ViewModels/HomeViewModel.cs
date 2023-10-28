0a3f8ecac86c080a2db91060b9b31e366060bfbd
GineTik <densheva18072003@gmail.com>
2023-10-28 15:32:53 +0300

    Fully implemented search functionality

--- src/UI/ViewModels/HomeViewModel.cs
+++ src/UI/ViewModels/HomeViewModel.cs
@@ -1,50 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 using Application.Queries;
 using Domain.APIModels;
 using MediatR;
+using Microsoft.Extensions.Configuration;
 using UI.ViewModels.Base;
 
 namespace UI.ViewModels;
 
 public class HomeViewModel : ViewModelBase
 {
-    private IEnumerable<Coin> _coins;
+    private ObservableCollection<Coin> _coins;
+    private string _searchText;
+    private readonly DispatcherTimer _searchTimer;
 
     private readonly IMediator _mediator;
 
-    public HomeViewModel(IMediator mediator)
+    public HomeViewModel(IMediator mediator, IConfiguration configuration)
     {
         _mediator = mediator;
-        _coins = Array.Empty<Coin>();
-        LoadCoins();
+        _coins = new ObservableCollection<Coin>();
+        _searchText = "";
+        _searchTimer = new DispatcherTimer();
+        _searchTimer.Tick += SearchTimerTick;
+        var interval = Convert.ToInt32(configuration["SearchIntervalInMilliseconds"]);
+        _searchTimer.Interval = TimeSpan.FromMilliseconds(interval);
+        
+        SearchCommand = new ViewModelCommand(SearchCoins);
+        SearchCommand.Execute(null);
     }
+
+    public ViewModelCommand SearchCommand { get; }
     
-    public IEnumerable<Coin> Coins
+    public ObservableCollection<Coin> Coins
     {
         get => _coins;
         private set => SetField(ref _coins, value);
     }
-    
-    private async Task LoadCoins()
+
+    public string SearchText
     {
-        try
+        get => _searchText;
+        set => SetField(ref _searchText, value);
+    }
+    
+    private void SearchCoins(object? parameter)
+    {
+        _searchTimer.Stop();
+        _searchTimer.Start();
+    }
+
+    private void SearchTimerTick(object? sender, EventArgs e)
+    {
+        _searchTimer.Stop();
+        Search();
+    }
+
+    private void Search()
+    {
+        Task.Run(async () =>
         {
-            var result = await _mediator.Send(new GetMostPopularCoinsQuery());
-            
+            var result = await _mediator.Send(new GetMostPopularCoinsQuery(SearchText));
+
             if (result.Successfully)
-            {
-                Coins = result.Content;
-            }
+                Coins = new ObservableCollection<Coin>(result.Content);
             else
-            {
                 Console.WriteLine(result.Exception!.Message);
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-        }
+        });
     }
 }
--- src/UI/Views/HomePage.xaml.cs
+++ src/UI/Views/HomePage.xaml.cs
@@ -1,12 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Windows.Controls;
-using Domain.APIModels;
-using UI.ViewModels;
+﻿using UI.ViewModels;
 
 namespace UI.Views;
 
-public partial class HomePage : Page
+public partial class HomePage
 {
     public HomePage(HomeViewModel homeViewModel)
     {
